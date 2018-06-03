400f270ed300ac37a7f714b80cdc98b15c6b7279
Jake Stothard <stothardj@gmail.com>
2018-06-03 00:50:11 -0700

    Get upcoming events from meetup

--- offbeat.py
+++ offbeat.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, json
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,6 +17,12 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/upcomingevents')
+def upcomingevents():
+    response = requests.get('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Offbeat-Fun&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=214474886&sig=dad844af55cda43bf7e0ea2e09ea1e9dafd2863d').content
+    events = json.loads(response)
+    return render_template('upcomingevents.html', events=events)
 
 if __name__ == '__main__':
     app.run(debug = True, use_reloader = True)
---
+++ static/events.js
@@ -0,0 +1,19 @@
+console.log('yay');
+
+function displayUpcomingEvents() {
+    const upcomingeventsEl = document.getElementById('upcomingevents');
+    if (this.status != 200) {
+	upcomingeventsEl.textContent = 'Failed to retrieve upcoming events. Please check Meetup directly';
+    }
+    // TODO: Seems dubious to set innerHTML based on http response
+    upcomingeventsEl.innerHTML = this.responseText;
+}
+
+function fetchUpcomingEvents() {
+    const req = new XMLHttpRequest();
+    req.addEventListener('load', displayUpcomingEvents);
+    req.open('GET', '/upcomingevents');
+    req.send();
+}
+
+fetchUpcomingEvents();
