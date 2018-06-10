36fb4a7992d9fa01e8b43f76a0b25ecbf7aea378
Jake Stothard <stothardj@gmail.com>
2018-06-10 01:58:02 -0700

    Add more info about events

--- offbeat.py
+++ offbeat.py
@@ -1,5 +1,7 @@
 import requests
 from flask import Flask, render_template, json
+from datetime import datetime, timedelta
+from babel.dates import format_datetime
 app = Flask(__name__)
 
 @app.route('/')
@@ -18,10 +20,27 @@ def about():
 def contact():
     return render_template('contact.html')
 
+def format_event(ev):
+    # TODO: timezones and locale
+    utc_time = datetime.fromtimestamp(ev['time'] // 1000)
+    start_time = format_datetime(utc_time, locale='en')
+    duration = timedelta(milliseconds = ev['duration'])
+    end_time = format_datetime(utc_time + duration, locale='en')
+    return {
+        'name': ev['name'],
+        'description': ev['description'],
+        'start_time': start_time,
+        'end_time': end_time,
+        'yes_rsvp_count': ev['yes_rsvp_count'],
+        'rsvp_limit': ev.get('rsvp_limit', 'unlimited'),
+        'waitlist_count': ev['waitlist_count'],
+    }
+
 @app.route('/upcomingevents')
 def upcomingevents():
-    response = requests.get('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Offbeat-Fun&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=214474886&sig=dad844af55cda43bf7e0ea2e09ea1e9dafd2863d').content
-    events = json.loads(response)
+    response_json = requests.get('https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=Offbeat-Fun&page=200&fields=&order=time&desc=false&status=upcoming&sig_id=214474886&sig=dad844af55cda43bf7e0ea2e09ea1e9dafd2863d').content
+    response = json.loads(response_json)
+    events = [format_event(ev) for ev in response['results']]
     return render_template('upcomingevents.html', events=events)
 
 if __name__ == '__main__':
